4fd106bc2c5476bd2e2daec8170701c3b793a2fd
Exodin1 <exodin1@outlook.com>
2022-05-30 14:35:31 -0300

    feat: creating orders models and migrations

---
+++ models/orders.js
@@ -0,0 +1,28 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const orders = sequelize.define({
+    orderNfId: DataTypes.STRING,
+    orderNumber: DataTypes.STRING,
+    orderPath: DataTypes.STRING,
+    orderFileName: DataTypes.STRING,
+    orderOriginalName: DataTypes.STRING,
+    emissionDate: DataTypes.STRING,
+    pdfFile: DataTypes.STRING,
+    emitedTo: DataTypes.STRING,
+    nNf: DataTypes.STRING,
+    CTE: DataTypes.STRING,
+    value: DataTypes.STRING,
+    cnpjId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    buyerId: DataTypes.INTEGER,
+    providerId: DataTypes.INTEGER,
+    orderStatusBuyer: DataTypes.STRING,
+    orderStatusProvider: DataTypes.STRING,
+    deliveryReceipt: DataTypes.STRING,
+    cargoPackingList: DataTypes.STRING,
+    deliveryCtrc: DataTypes.STRING
+  }, {
+    modelName: 'orders',
+  })
+  return orders
+}
