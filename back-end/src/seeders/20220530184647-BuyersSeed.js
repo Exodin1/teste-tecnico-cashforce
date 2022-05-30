f6b5493151f48f8fa940ab4238ffbb62fcd68db8
Exodin1 <exodin1@outlook.com>
2022-05-30 15:52:08 -0300

    feat: creating buyers seed

---
+++ back-end/src/seeders/20220530184647-BuyersSeed.js
@@ -0,0 +1,36 @@
+'use strict'
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('buyers', [{
+      id: 1,
+      name: 'SACADO 001',
+      tradingName: 'SACADO 001 LTDA',
+      cashforceTax: '0',
+      responsibleName: null,
+      responsibleEmail: null,
+      responsiblePosition: null,
+      responsiblePhone: null,
+      responsibleMobile: null,
+      website: null,
+      postalCode: null,
+      address: null,
+      number: null,
+      complement: null,
+      neighborhood: null,
+      city: null,
+      state: null,
+      phoneNumber: null,
+      situation: null,
+      situationDate: null,
+      createdAt: '2020-10-29 21:20:33',
+      updatedAt: '2020-10-29 21:20:34',
+      cnpjId: 1,
+      confirm: 1,
+      email: null
+    }])
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('buyers', null, {})
+  }
+}
