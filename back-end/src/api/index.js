34a4604320b1b7a5b76c2b21e27d62733bc94f87
Exodin1 <exodin1@outlook.com>
2022-05-31 12:34:26 -0300

    feat: import controller func to index.js

--- back-end/src/api/index.js
+++ back-end/src/api/index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 require('dotenv').config()
-
+const { getAllBuyers } = require('./controllers/buyersController')
 const app = express()
 const port = process.env.PORT || 3002
 
 app.use(express.json())
 app.listen(port, () => console.log(`Server running on port ${port}`))
 
-app.get('/buyers', (req, res) => {
-  res.send('get buyers')
-})
+app.get('/buyers', getAllBuyers)
 
 app.get('/providers', (req, res) => {
   res.send('get providers')
