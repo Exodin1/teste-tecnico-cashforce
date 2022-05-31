00f6d272e0fc835a5dbe17c57fc3af02cb35aafb
Exodin1 <exodin1@outlook.com>
2022-05-31 09:42:34 -0300

    feat: require dotenv in index.js

--- back-end/src/api/index.js
+++ back-end/src/api/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3002
 
 app.use(express.json())
 app.listen(port, () => console.log(`Server running on port ${port}`))
